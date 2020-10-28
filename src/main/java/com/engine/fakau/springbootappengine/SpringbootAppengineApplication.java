ea1845394a201738180b8a8d31097bdf1c45c2aa
Laurent Kafka M <laurentkafka123@gmail.com>
2020-10-28 10:41:50 -0400

    init

---
+++ src/main/java/com/engine/fakau/springbootappengine/SpringbootAppengineApplication.java
@@ -0,0 +1,13 @@
+package com.engine.fakau.springbootappengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAppengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAppengineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/engine/fakau/springbootappengine/SpringbootAppengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engine.fakau.springbootappengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAppengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
