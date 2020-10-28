f9ed5ea556c0b20c1a1b05409d945779ea9fbcd2
Laurent Kafka M <laurentkafka123@gmail.com>
2020-10-28 11:33:56 -0400

    add, web dependency in pom.xml

--- src/main/java/com/engine/fakau/springbootappengine/SpringbootAppengineApplication.java
+++ src/main/java/com/engine/fakau/springbootappengine/SpringbootAppengineApplication.java
@@ -2,12 +2,21 @@ package com.engine.fakau.springbootappengine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/api")
 public class SpringbootAppengineApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootAppengineApplication.class, args);
+	}
+	@GetMapping("/test")
+	public String helloWorld(){
+		return "Hello world, the test works  successfully";
 	}
 
 }
